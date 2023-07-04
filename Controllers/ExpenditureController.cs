b92562acfc1c20685db8cde14bd9a01433ef0427
victor <fanneauv@gmail.com>
2023-07-04 16:51:14 +0200

    upload

---
+++ Controllers/ExpenditureController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        public List<Expenditure> ExpenditureLog = new List<Expenditure>();
+
+        public ExpenditureController()
+        {
+            this.ExpenditureLog.Add(new Expenditure("lait", DateTime.Parse("22/12/2022"), 5.20));
+            this.ExpenditureLog.Add(new Expenditure("cookies", DateTime.Parse("23/12/2022"), 4.70));
+            this.ExpenditureLog.Add(new Expenditure("allume-feu", DateTime.Parse("24/12/2022"), 15));
+        }
+
+        // GET: ExpenditureController
+        public ActionResult Index()
+        {
+            return View(ExpenditureLog);
+        }
+    }
+}
---
+++ Models/Expenditure.cs
@@ -0,0 +1,16 @@
+﻿namespace Razor.Models
+{
+    public class Expenditure
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+
+        public Expenditure(string Name, DateTime Date, double Price)
+        {
+            this.Name = Name;
+            this.Date = Date;
+            this.Price = Price;
+        }
+    }
+}
